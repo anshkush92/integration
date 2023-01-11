39dd9f3b6e473d2c6a2db9c37df6c8a568789081
anshkush92college <imt_2020013@iiitm.ac.in>
2023-01-11 18:46:50 +0530

    Changed the name of the app

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             alt="Flowbite Logo"
           />
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-            Stripe Checkout
+            Integration
           </span>
         </a>
 
