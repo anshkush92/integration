7770e99ebdd21d7855fc7f25e3fe47f4dad1b6db
anshkush92college <imt_2020013@iiitm.ac.in>
2023-01-12 11:44:02 +0530

    Now also able to add the address / delivery options in the stripe checkout

--- server/controllers/stripe/stripe.controller.js
+++ server/controllers/stripe/stripe.controller.js
@@ -30,6 +30,37 @@ const createCheckoutSession = async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA', 'IN', 'GB', 'AU', 'NZ', 'IE', 'SG'],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: { amount: 0, currency: 'usd' },
+          display_name: 'Free shipping',
+          delivery_estimate: {
+            minimum: { unit: 'business_day', value: 5 },
+            maximum: { unit: 'business_day', value: 7 },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: { amount: 1500, currency: 'usd' },
+          display_name: 'Next Day Air',
+          // Delivers in exactly 1 day
+          delivery_estimate: {
+            minimum: { unit: 'business_day', value: 1 },
+            maximum: { unit: 'business_day', value: 1 },
+          },
+        },
+      },
+    ],
+    phone_number_collection: {
+      enabled: true,
+    },
     line_items: lineItems,
     mode: 'payment',
     success_url: `${CLIENT_URL}/success`,
